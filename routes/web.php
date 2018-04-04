bedd6a440ebb2258ccff5f4d5973015758afc3f4
Harun R Rayhan <harun.b13@gmail.com>
2018-04-04 19:32:02 +0600

    Added pusher dispatcher

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,14 @@ use App\Events\OrderStatusUpdated;
 |
 */
 
+class Order{
+	public $id;
+	public function __construct($id) {
+		$this->id = $id;
+	}
+}
+
 Route::get('/', function () {
-	OrderStatusUpdated::dispatch();
+	OrderStatusUpdated::dispatch(new Order(1));
     return view('welcome');
 });
